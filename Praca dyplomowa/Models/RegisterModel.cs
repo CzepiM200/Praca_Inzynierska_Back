dc8d995b477287a0d2bd1ab7c1db2fdcb31f65ed
Marcin Czepiela <czepimm200@gmail.com>
2020-12-14 21:18:25 +0100

    Add Email to RegisterModel

--- Praca dyplomowa/Models/RegisterModel.cs
+++ Praca dyplomowa/Models/RegisterModel.cs
@@ -14,6 +14,9 @@ namespace Praca_dyplomowa.Models
         public string UserName { get; set; }
 
         [Required]
+        public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
